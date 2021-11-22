60136714c8abc6bb434f5889d7649dd10a17d350
msg4edson <msg.edson@gmail.com>
2021-11-22 13:14:18 -0300

    merging all files

---
+++ 3-scripting-for-building/simple-ts-server/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 4005;
+let runningMessage = 'Server is running on port ' + port;
+app.get('/', (req, res) => {
+    console.log('API was sucessfully requested');
+    res.send(runningMessage + ' hello Edson!');
+});
+const server = app.listen(port, () => {
+    console.log(runningMessage);
+});
+module.exports = server;
